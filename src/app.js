8a948e01695bf09426ecc873cc3aca90c798601d
primahafiz <primahafiz@gmail.com>
2022-07-31 14:55:53 +0700

    change config

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://bnmo-app.herokuapp.com/",
       credentials: true,
     })
   );
@@ -34,8 +34,8 @@ app.use(session({
     secret : SESS_SECRET,
     cookie : {
         maxAge : SESS_LIFETIME,
-        sameSite : 'lax',
-        secure : false // pas production jadi true
+        sameSite : 'none',
+        secure : true
     }
 }))
 
